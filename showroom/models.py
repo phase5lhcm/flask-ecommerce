22700d0fb52a516e54d1e483f0457bf16b413832
cmaynard <cmaynard@cvent.com>
2022-01-23 18:59:02 -0500

    Add purchase and sell functionality

--- showroom/models.py
+++ showroom/models.py
@@ -2,6 +2,8 @@ from wtforms.validators import Length
 from showroom import db, login_manager
 from showroom import bcrypt
 from flask_login import UserMixin
+from sqlalchemy.orm import backref, relationship
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,7 +15,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(),nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000) 
-    # products = db.relationship('Product', backref='owner',lazy=True)
+    # purchased_id = db.Column(db.String(), db.ForeignKey('product.id'))
+    products = db.relationship('Product', backref='owner',lazy=True)
     
     @property
     def refactor_budget(self):
@@ -39,6 +42,13 @@ class User(db.Model, UserMixin):
     def can_purchase(self, item_obj):
         return self.budget >= item_obj.price
 
+    def can_sell(self, item_obj):
+        return item_obj in self.products
+    
+
+    def __repr__(self):
+        return f'User: {self.username}'
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,12 +56,19 @@ class Product(db.Model):
     price = db.Column(db.Integer, nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False,unique=True)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
-    owner_id = db.Column(db.String(), db.ForeignKey('user.id'))
-    
+    owner_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    # owner = relationship("User", backref=backref('product'))
+ 
     def __repr__(self):
-        return f'Product {self.name}'
+        return f'Product: {self.name}'
     
     def confirm_purchase(self, user):
-        self.owned_id = user.id
+        self.owner_id = user.id
         user.budget -= self.price
         db.session.commit()
+
+    def confirm_sale(self, user):
+        self.owner_id = None
+        user.budget += self.price
+        db.session.commit()
+
--- showroom/routes.py
+++ showroom/routes.py
@@ -2,10 +2,10 @@ from urllib import request
 from showroom import app
 from flask import render_template, redirect, flash,url_for, request
 from showroom.models import Product, User
-from showroom.forms import RegisterForm, LoginForm, PurchaseForm
+from showroom.forms import RegisterForm, LoginForm, PurchaseForm, SellForm
 from showroom import db
 from flask_login import login_user, logout_user, login_required, current_user
-
+from showroom.models import User
 #TODO - add logging info to file when used in production
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -18,33 +18,42 @@ def home_page():
 @login_required
 def showroom():
     purchase_form = PurchaseForm()
-    if purchase_form.is_submitted():
-        print("form submitted")
+    sale_form = SellForm()
+    # if purchase_form.validate_on_submit():
+        # print(purchase_form.__dict__)
+        # print(purchase_form['submitBtn'])
+  
     # request method checks removes resumbit 
     # form question when refreshing page
-    # if purchase_form.validate_on_submit():
     if request.method == "POST":
+        #Purchase item logic
         purchased_item = request.form.get('purchased_item')
-        app.logger.info(f'purchase form selected', request.form.get('purchased_item'))
-        print(f"form: ", request.form.get('purchased_item'))
+        # app.logger.info(f'purchase form selected', request.form.get('purchased_item'))
+        # print(f"form: ", request.form.get('purchased_item'))
         p_item_obj = Product.query.filter_by(name=purchased_item).first()
         if p_item_obj:
             if current_user.can_purchase(p_item_obj):
                 p_item_obj.confirm_purchase(current_user)
-                app.logger.info(f'purchase form selected by: {current_user.username} for {p_item_obj.name} at ${p_item_obj.price} dollars')
+                app.logger.info(f'purchase initiated by: {current_user.username} for {p_item_obj.name} at ${p_item_obj.price} dollars')
                 flash(f"Purchase successful for {p_item_obj.name} at {p_item_obj.price} dollars", category="success")
             else:
                 flash("Your budget is too low for this item", category="danger")
+        #sell item login
+        sold_product = request.form.get('sold_item')
+        s_product_obj = Product.query.filter_by(name=sold_product).first()
+        if s_product_obj:
+            if current_user.can_sell(s_product_obj):
+                s_product_obj.confirm_sale(current_user)
+                flash(f"Sale successful for {s_product_obj.name} at {s_product_obj.price} dollars", category="success")
+            else:
+                flash("Unable to sell this item", category="danger")
         return redirect(url_for('showroom'))
-    #let's return all Products in db
-    # passing owner_id=None ensures that only items that
-    # have not been purchased yet are displayed
-    # in the PRODUCTS AVAILABLE section
-    products = Product.query.filter_by(owner=None)
-    owned_products = Product.query.filter_by(owner = current_user.id)
-    print("user", current_user.id)
-    # print(f"owned products {owned_products.__dict__}")
-    return render_template('showroom.html', products=products, purchase_form=purchase_form, owned_products=owned_products)
+    #let's return all Products in db purchased by this user
+    products = Product.query.all()
+    owned_products = Product.query.filter_by(owner_id = current_user.id)
+    for owned_product in owned_products:
+        print(f"owned: {owned_product.name}")
+    return render_template('showroom.html', products=products, purchase_form=purchase_form, owned_products=owned_products, sale_form=sale_form)
 
 @app.route("/register", methods=['GET','POST'])
 def register_page():
@@ -55,8 +64,8 @@ def register_page():
                            password=form.password1.data)
         db.session.add(create_user)
         db.session.commit()
-        # print(f"user: create_user")
-        # app.logger.info(f'user', create_user)
+        print(f"user: create_user")
+        app.logger.info(f'user', create_user)
         login_user(create_user)
         flash(f"Welcome! You are now logged in as {create_user.username}", category="success")
         return redirect(url_for('showroom'))
